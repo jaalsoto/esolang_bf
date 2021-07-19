cc12f12143f78f18bb1233f908f5c058c8cb0627
jaalsoto <jaalsoto@users.noreply.github.com>
2021-07-19 09:07:50 -0700

    Updated C code style.

--- brainfuck.c
+++ brainfuck.c
@@ -7,155 +7,192 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define ADDR_MAX 30000
 
 int
-main (int argc, const char *argv[])
+main (int argc, char *argv[])
 {
-    const int ADDRESS_SPACE = 30000;
-    FILE *source;
-    FILE *output;
-    char *instruction_memory;
-    uint8_t memory[ADDRESS_SPACE] = {0};
-    int ret;
-    int depth = 0;
-    unsigned int memory_index = 0;
-    unsigned int length = 0;
-    unsigned int program_counter = 0;
+    FILE *in, *out;
+    unsigned char *code, cells[ADDR_MAX];
+    int ret, depth, idx, pc, max, rem;
+    size_t len;
     char input;
 
     if (argc != 2) {
-        printf ("usage: <input file>\n");
+        fprintf (stderr, "usage: <input>\n");
         exit (1);
     }
 
-    output = fopen ("memory.dmp", "w");
-    if (output == NULL) {
-        printf ("failed to open memory.dmp\n");
+    memset (cells, 0, sizeof (cells));
+
+    depth = idx = max = 0;
+
+    /*
+     *  Load Brainfuck program into memory.
+     */
+
+    out = fopen ("memory", "w");
+    if (out == NULL) {
+        fprintf (stderr, "unable to open memory file\n");
         exit (1);
     }
 
-    source = fopen (argv[1], "r");
-    if (source == NULL) {
-        printf ("failed to open %s\n", argv[1]);
+    in = fopen (argv[1], "r");
+    if (in == NULL) {
+        fprintf (stderr, "unable to open %s\n", argv[1]);
         exit (1);
     }
 
-    fseek (source, 0, SEEK_END);
-    length = ftell (source);
-    rewind (source);
+    fseek (in, 0, SEEK_END);
+    len = ftell (in);
+    rewind (in);
 
-    instruction_memory = malloc (sizeof (char) * length);
-    if (instruction_memory == NULL) {
-        printf ("memory allocation failure\n");
+    code = malloc (len);
+    if (code == NULL) {
+        fprintf (stderr, "unable to allocate memory\n");
         exit (1);
     }
 
-    ret = fread (instruction_memory, sizeof (char), length, source);
-    if (ret != length) {
-        printf ("failed to read %s\n", argv[1]);
+    ret = fread (code, 1, len, in);
+    if (ret != len) {
+        fprintf (stderr, "error reading %s\n", argv[1]);
         exit (1);
     }
 
-    fclose (source);
+    fclose (in);
 
-    for (program_counter = 0; program_counter < length; program_counter++) {
-        switch (instruction_memory[program_counter]) {
-            case '[': depth++; break;
-            case ']': depth--; break;
-            default: break;
+    /*
+     *  Verify the syntax of the loop instructions are valid.
+     */
+
+    for (pc = 0; pc < len; pc++) {
+        switch (code[pc]) {
+        case '[':
+            depth++;
+            break;
+        case ']':
+            depth--;
+            break;
         }
         if (depth < 0) {
-            printf ("loop instruction syntax error\n");
+            fprintf (stderr, "loop instruction syntax error\n");
             exit (1);
         }
     }
-
     if (depth != 0) {
-        printf ("loop instruction syntax error\n");
+        fprintf (stderr, "loop instruction syntax error\n");
         exit (1);
     }
 
-    for (program_counter = 0; program_counter < length; program_counter++) {
-        switch (instruction_memory[program_counter]) {
-            case '>':
-                if (memory_index == ADDRESS_SPACE - 1) {
-                    printf ("upper out of bounds pointer\n");
-                    exit (1);
+    /*
+     *  Start the instruction cycle.
+     */
+
+    for (pc = 0; pc < len; pc++) {
+        switch (code[pc]) {
+
+        case '>':
+            if (idx == ADDR_MAX - 1) {
+                fprintf (stderr, "upper out of bounds pointer\n");
+                exit (1);
+            }
+            idx++;
+            if (idx > max)
+                max = idx;
+            break;
+        case '<':
+            if (idx == 0) {
+                fprintf (stderr, "lower out of bounds pointer\n");
+                exit (1);
+            }
+            idx--;
+            break;
+
+        /*
+         *  If the loop body needs to be skipped, traverse forward through
+         *  instructions tracking the loop depth. When relative loop depth
+         *  reaches 0, exit.
+         */
+
+        case '[':
+            if (cells[idx] == 0) {
+                depth = 1;
+                pc++;
+            }
+            while (depth) {
+                switch (code[pc]) {
+                case '[':
+                    depth++;
+                    break;
+                case ']':
+                    depth--;
+                    break;
                 }
-                memory_index++;
-                break;
-            case '<':
-                if (memory_index == 0) {
-                    printf ("lower out of bounds pointer\n");
-                    exit (1);
+                if (depth != 0)
+                    pc++;
+            }
+            break;
+
+        /*
+         *  If another iteration of the loop body is required, traverse back
+         *  through instructions tracking the loop depth. When relative loop
+         *  depth reaches 0, exit.
+         */
+
+        case ']':
+            if (cells[idx] >= 1) {
+                depth = 1;
+                pc--;
+            }
+            while (depth) {
+                switch (code[pc]) {
+                case '[':
+                    depth--;
+                    break;
+                case ']':
+                    depth++;
+                    break;
                 }
-                memory_index--;
-                break;
-            case '[':
-                if (memory[memory_index] == 0) {
-                    depth = 1;
-                    program_counter++;
-                }
-                while (depth) {
-                    switch (instruction_memory[program_counter]) {
-                        case '[': depth++; break;
-                        case ']': depth--; break;
-                        default: break;
-                    }
-                    if (depth != 0)
-                        program_counter++;
-                }
-                break;
-            case ']':
-                if (memory[memory_index] >= 1) {
-                    depth = 1;
-                    program_counter--;
-                }
-                while (depth) {
-                    switch (instruction_memory[program_counter]) {
-                        case '[': depth--; break;
-                        case ']': depth++; break;
-                        default: break;
-                    }
-                    if (depth != 0)
-                        program_counter--;
-                }
-                break;
-            case '.':
-                putc (memory[memory_index],stdout);
-                break;
-            case ',':
-                input = fgetc (stdin);
-                fseek (stdin, 0, SEEK_END);
-                if (input != EOF)
-                    memory[memory_index] = input;
-                break;
-            case '+':
-                if (memory[memory_index] != 255)
-                    memory[memory_index]++;
-                else
-                    memory[memory_index] = 0;
-                break;
-            case '-':
-                if (memory[memory_index] != 0)
-                    memory[memory_index]--;
-                else
-                    memory[memory_index] = 255;
-                break;
-            default:
-                break;
+                if (depth != 0)
+                    pc--;
+            }
+            break;
+
+        case '.':
+            putc (cells[idx], stdout);
+            break;
+        case ',':
+            input = fgetc (stdin);
+            fflush (stdin);
+            if (input != EOF)
+                cells[idx] = input;
+            break;
+        case '+':
+            cells[idx]++;
+            break;
+        case '-':
+            cells[idx]--;
+            break;
         }
     }
 
-    ret = fwrite (memory, sizeof (uint8_t), ADDRESS_SPACE, output);
-    if (ret != ADDRESS_SPACE) {
-        printf ("failed to write memory.dmp\n");
+    /*
+     *  Print the values of the memory cells used.
+     */
+    
+    rem = max % 16;
+    max += (16 - rem);
+
+    ret = fwrite (cells, 1, max, out);
+    if (ret != max) {
+        fprintf (stderr, "error writing memory file\n");
         exit (1);
     }
 
-    fclose (output);
-
-    free (instruction_memory);
+    free (code);
+    fclose (out);
 
     return (0);
 }
